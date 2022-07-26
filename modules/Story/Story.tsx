acb8c330b8ec52049acdcb0418455d4b73abeecd
Oleg Semyonov <oleg@prezly.com>
2022-07-26 15:23:37 +0400

    Remove unused full width image styles

--- modules/Story/Story.tsx
+++ modules/Story/Story.tsx
@@ -23,9 +23,6 @@ type Props = {
     story: ExtendedStory;
 };
 
-// TODO: This will become a theme setting
-const IS_FANCY_IMAGE_ENABLED = false;
-
 function Story({ story }: Props) {
     const { showDate } = useThemeSettings();
     const { isTablet } = useDevice();
@@ -47,8 +44,7 @@ function Story({ story }: Props) {
             <article className={styles.story}>
                 <div
                     className={classNames(styles.container, {
-                        [styles.withImage]: hasHeaderImage && !IS_FANCY_IMAGE_ENABLED,
-                        [styles.withFullWidthImage]: hasHeaderImage && IS_FANCY_IMAGE_ENABLED,
+                        [styles.withImage]: hasHeaderImage,
                     })}
                 >
                     {hasCategories && (
@@ -71,10 +67,7 @@ function Story({ story }: Props) {
                     {headerImage && (
                         <Image
                             alt=""
-                            className={classNames({
-                                [styles.mainImage]: !IS_FANCY_IMAGE_ENABLED,
-                                [styles.fullWidthImage]: IS_FANCY_IMAGE_ENABLED,
-                            })}
+                            className={styles.mainImage}
                             objectFit="cover"
                             layout="fill"
                             imageDetails={headerImage}
