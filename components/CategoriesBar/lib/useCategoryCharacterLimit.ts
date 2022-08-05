779a9c9e25642593352a8eb7c0626f40c269d0f3
Oleg Semyonov <oleg@prezly.com>
2022-08-05 14:30:42 +0400

    Tweak character limits for different screens

--- components/CategoriesBar/lib/useCategoryCharacterLimit.ts
+++ components/CategoriesBar/lib/useCategoryCharacterLimit.ts
@@ -4,13 +4,21 @@ export function useCategoryCharacterLimit() {
     const { width } = useWindowSize(undefined, true);
 
     if (width >= 1200) {
-        return 120;
+        return 150;
     }
 
-    if (width > 500) {
+    if (width >= 700 && width <= 750) {
+        return 65;
+    }
+
+    if (width > 600) {
         // We can fit about 10 extra characters per extra 100 pixels of screen width
         return Math.floor(width / 100) * 10;
     }
 
-    return 40;
+    if (width > 500) {
+        return 45;
+    }
+
+    return 30;
 }
