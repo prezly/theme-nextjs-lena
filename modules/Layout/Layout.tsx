e0a81b96b28a824f65f217e9c1d7fcf3b20492b6
christianprezly <christian@prezly.com>
2026-08-10 16:37:06 -0300

    visibility fix
    visibility fix

--- modules/Layout/Layout.tsx
+++ modules/Layout/Layout.tsx
@@ -39,8 +39,6 @@ const CookieConsentBar = dynamic(() => import('./CookieConsentBar'), {
 
 const PATHS_WITH_CUSTOM_BG = ['/', '/[slug]', '/s/[slug]', '/media', '/media/album/[uuid]'];
 const STORY_PAGE_PATHS = ['/[slug]', '/s/[slug]'];
-const noIndex = process.env.VERCEL === '1';
-
 function Layout({ children, description, imageUrl, title, hasError }: PropsWithChildren<Props>) {
     const [isLoadingPage, setIsLoadingPage] = useState(false);
     const { page } = useAnalytics();
@@ -98,13 +96,7 @@ function Layout({ children, description, imageUrl, title, hasError }: PropsWithC
         <>
             <Tracking />
             <Branding newsroom={newsroom} />
-            <PageSeo
-                title={title}
-                description={description}
-                imageUrl={imageUrl}
-                noindex={noIndex}
-                nofollow={noIndex}
-            />
+            <PageSeo title={title} description={description} imageUrl={imageUrl} />
             <NotificationsBar notifications={displayedNotifications} />
             <CookieConsentBar />
             <div
